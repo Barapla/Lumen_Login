7453195b10d3c18e25b11f40450b246471c01395
Bryan Araujo <MY_NAME@example.com>
2021-08-26 20:24:54 -0700

    initial Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Crypt;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function index()
+    {
+     
+     $User= User::all();
+
+     return response()->json($user
+    );
+
+    }
+
+     public function create(Request $request)
+     {
+        $User = new User;
+
+       $User->name= $request->name;
+       $User->email = $request->email;
+       $User->password= $request->password;
+       
+       $User->save();
+
+       return response()->json($User);
+     }
+     public function autenticate(Request $request){
+         $User = new User;
+         $token = $request->bearerToken();
+     }
+
+   
+    }
